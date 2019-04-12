453263e4ec62f7f6c59eaa26da7d9e5a72d4132f
Gekif <dzulfikar.maulana@gmail.com>
2019-04-12 10:50:11 +0700

    01-Getting Started: Your First Node Program

---
+++ first-app/app.js
@@ -0,0 +1,26 @@
+/**
+ * JavaScript Function
+ * @param name
+ */
+function sayHello(name) {
+    console.log('Hello ' + name)
+}
+
+// sayHello('Fikar');
+console.log(window);
+
+
+
+
+
+/**
+ * Ecmascript Function
+ * @param name
+ */
+/*
+const sayHello = (name) => {
+    console.log('Hello ' + name);
+};
+
+sayHello('Fikar');
+*/
